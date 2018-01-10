a6c6cf1f9f86b834920b96fc0561e5899ca661b4
jzs <jinzhongshuai@antiy.cn>
2018-01-10 16:28:24 +0800

    update index.test

--- test/index.test.js
+++ test/index.test.js
@@ -1,13 +1,47 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { render, mount } from 'enzyme';
 import TreeTransfer from '../src';
+
+const treeTransferProps = {
+  source: [
+    {
+      key: '0',
+      title: '0',
+      children: [
+        {
+          key: '0-0',
+          title: '0-0',
+        },
+        {
+          key: '0-1',
+          title: '0-1',
+        }
+      ]
+    }
+  ],
+  target: ['0-1']
+};
 
 describe('TreeTransfer', () => {
   it('renders correctly', () => {
-    const wrapper = shallow(
-      <TreeTransfer />
-    );
-    expect(wrapper.find('.lucio-tree-transfer').exists()).toBeTruthy();
+    const wrapper = render(<TreeTransfer {...treeTransferProps} />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should move all selected tree leafs to right list', () => {
+    const handleChange = jest.fn();
+    const wrapper = mount(<TreeTransfer {...treeTransferProps} onChange={handleChange} />);
+    wrapper.find(TreeTransfer).find('.ant-tree').find('.ant-tree-checkbox').at(0).simulate('click');
+    wrapper.find(TreeTransfer).find('.tree-transfer-operation').find('button').at(0).simulate('click');
+    expect(handleChange).toHaveBeenLastCalledWith(['0-0', '0-1']);
+  });
+
+  it('should move unselected tree leafs to right list', () => {
+    const handleChange = jest.fn();
+    const wrapper = mount(<TreeTransfer {...treeTransferProps} onChange={handleChange} />);
+    wrapper.find(TreeTransfer).find('.ant-tree').find('.ant-tree-checkbox').at(2).simulate('click');
+    wrapper.find(TreeTransfer).find('.tree-transfer-operation').find('button').at(0).simulate('click');
+    expect(handleChange).toHaveBeenLastCalledWith([]);
   });
 });
   
